9dc9551ecf5d5a23699b929080f7950af7479331
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-22 20:52:05 -0600

    Creating Department API Controler

---
+++ CRMAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CRMAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private IDepartmentRepository _departmentRepo;
+        private readonly IMapper _mapper;
+
+        public DepartmentController(IDepartmentRepository departmentrepo, IMapper mapper)
+        {
+            _departmentRepo = departmentrepo;
+            _mapper = mapper;
+        }
+    }
+}
