836855859a95cfcb4d761ababdf01c90ac2a87c3
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-22 19:17:49 -0600

    Adding Department model and configuring DB connection

---
+++ CRMAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRMAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ CRMAPI/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; } = true;
+    }
+}
