6b3b8eb51e357e5e381c177f4ea914783f5b9a19
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-22 20:32:28 -0600

    Creating Deparment DTO

---
+++ CRMAPI/Models/Dtos/DepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models.Dtos
+{
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
