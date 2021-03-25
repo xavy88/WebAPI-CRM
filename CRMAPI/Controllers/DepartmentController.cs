e5a4b2d162ac6bac79ac12797fb58c1feea9da22
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-24 19:14:06 -0600

    Adding Active and inactive method into Department controller

--- CRMAPI/Controllers/DepartmentController.cs
+++ CRMAPI/Controllers/DepartmentController.cs
@@ -89,11 +89,51 @@ namespace CRMAPI.Controllers
                 return BadRequest(ModelState);
             }
             var departmentObj = _mapper.Map<Department>(departmentDto);
-           // departmentObj.IsActive = false;
-
+           
             if (!_departmentRepo.UpdateDepartment(departmentObj))
             {
                 ModelState.AddModelError("", $"Something went wrong when updating the record {departmentObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpPatch("{departmentId:int}", Name = "InactiveDepartment")]
+        public IActionResult InactiveDepartment(int departmentId, [FromBody] DepartmentDto departmentDto)
+        {
+            if (departmentDto == null || departmentId != departmentDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            var departmentObj = _mapper.Map<Department>(departmentDto);
+            departmentObj.IsActive = false;
+
+            if (!_departmentRepo.InactiveDepartment(departmentObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when inactiving the record {departmentObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
+
+
+        [HttpPatch("{departmentId:int}", Name = "ActiveDepartment")]
+        public IActionResult ActiveDepartment(int departmentId, [FromBody] DepartmentDto departmentDto)
+        {
+            if (departmentDto == null || departmentId != departmentDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            var departmentObj = _mapper.Map<Department>(departmentDto);
+            departmentObj.IsActive = true;
+
+            if (!_departmentRepo.ActiveDepartment(departmentObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when activing the record {departmentObj.Name}");
                 return StatusCode(500, ModelState);
             }
 
