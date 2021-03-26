361db14bd5cbf25a38ba7a4f69e196ab90fc2fb8
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-25 20:03:17 -0600

    Adding Position Model and DTO

---
+++ CRMAPI/Models/Dtos/PositionDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models.Dtos
+{
+    public class PositionDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        public virtual DepartmentDto Department { get; set; }
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CRMAPI/Models/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models
+{
+    public class Position
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+    }
+}
