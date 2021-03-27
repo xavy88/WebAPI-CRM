57e7437ea0c811daab80100fd9d8f4c448f0d012
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-27 10:42:31 -0600

    Adding CRM Web Project

---
+++ CRMWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRMWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
