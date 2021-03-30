7c546ea688c9b7e3fdd4b940fc41a1f25cb5eccc
Xavy Urbina <xavyurbina88@gmail.com>
2021-03-29 19:40:20 -0600

    Adding Model Position-Department to web proyect

---
+++ CRMWeb/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMWeb.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; } = true;
+
+    }
+}
---
+++ CRMWeb/Models/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMWeb.Models
+{
+    public class Position
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Department")]
+        [Required]
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+
+    }
+}
