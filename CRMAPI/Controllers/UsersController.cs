4914118035c2de57265e1b4390dd4a5cda6df1a3
Xavy Urbina <xavyurbina88@gmail.com>
2021-04-02 09:38:25 -0600

    Adding Authenticate method

---
+++ CRMAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using CRMAPI.Models;
+using CRMAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepo.Authenticate(model.UserName, model.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            return Ok(user);
+        }
+    }
+}
