02ac729a690e6180860cf0e24721bc9e17b48aa9
Xavy Urbina <xavyurbina88@gmail.com>
2021-04-06 19:38:59 -0600

    Adding Employee Model & Dto

---
+++ CRMAPI/Models/Dtos/EmployeeDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models.Dtos
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Display(Name = "Hired Date")]
+        public DateTime HiredDate { get; set; }
+        public byte[] Image { get; set; }
+        [Display(Name = "Position")]
+        public int PositionId { get; set; }
+        [ForeignKey("PositionId")]
+        public virtual Position Position { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CRMAPI/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Display(Name = "Hired Date")]
+        public DateTime HiredDate { get; set; }
+        public byte[] Image { get; set; }
+        [Display(Name = "Position")]
+        public int PositionId { get; set; }
+        [ForeignKey("PositionId")]
+        public virtual Position Position { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
