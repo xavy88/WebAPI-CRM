1af99c4fd806babdc59ddeb1e47a7dc7e3ada465
Xavy Urbina <xavyurbina88@gmail.com>
2021-04-08 19:43:39 -0600

    Adding Account Model & DTO

---
+++ CRMAPI/Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Phone { get; set; }
+        [Required]
+        [Display(Name = "Sales Date")]
+        public DateTime SalesDate { get; set; }
+        [StringLength(300)]
+        public string Website { get; set; }
+        [StringLength(500)]
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
