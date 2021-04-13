795bd8d503c4f0f87ca91915c0a74895a6c3bdbb
Xavy Urbina <xavyurbina88@gmail.com>
2021-04-12 20:26:26 -0600

    Adding Service Model & Service DTOs

---
+++ CRMAPI/Models/Dtos/ServiceUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models.Dtos
+{
+    public class ServiceUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ CRMAPI/Models/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+    }
+}
