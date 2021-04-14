ea465198fac9b66ae5ad25536a3d8f4242460134
Xavy Urbina <xavyurbina88@gmail.com>
2021-04-13 20:09:00 -0600

    Adding Task Repository & Interface

---
+++ CRMAPI/Repository/IRepository/ITaskRepository.cs
@@ -0,0 +1,24 @@
+﻿using CRMAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Repository.IRepository
+{
+    public interface ITaskRepository
+    {
+        ICollection<Models.Task> GetTasks();
+        ICollection<Models.Task> GetTaskInDepartment(int departmentId);
+        Models.Task GetTask(int taskId);
+        bool TaskExists(string task);
+        bool TaskExists(int id);
+        bool CreateTask(Models.Task task);
+        bool UpdateTask(Models.Task task);
+        bool InactiveTask(Models.Task task);
+        bool ActiveTask(Models.Task task);
+        bool DeleteTask(Models.Task task);
+        bool Save();
+    }
+}
+
---
+++ CRMAPI/Repository/TaskRepository.cs
@@ -0,0 +1,84 @@
+﻿using CRMAPI.Data;
+using CRMAPI.Models;
+using CRMAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMAPI.Repository
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TaskRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool ActiveTask(Models.Task task)
+        {
+            _db.Tasks.Update(task);
+            return Save();
+        }
+
+        public bool CreateTask(Models.Task task)
+        {
+            _db.Tasks.Add(task);
+            return Save();
+        }
+
+        public bool DeleteTask(Models.Task task)
+        {
+            _db.Tasks.Remove(task);
+            return Save();
+        }
+
+        public bool TaskExists(string name)
+        {
+            bool value = _db.Tasks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool TaskExists(int id)
+        {
+            return _db.Tasks.Any(a => a.Id == id);
+        }
+
+        public Models.Task GetTask(int taskId)
+        {
+            return _db.Tasks.Include(c => c.Department).FirstOrDefault(a => a.Id == taskId);
+        }
+
+        public ICollection<Models.Task> GetTasks()
+        {
+            return _db.Tasks.Include(c => c.Department).Include(p=>p.Position).OrderBy(a => a.Name).ToList();
+        }
+
+        public bool InactiveTask(Models.Task task)
+        {
+            _db.Tasks.Update(task);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateTask(Models.Task task)
+        {
+            _db.Tasks.Update(task);
+            return Save();
+        }
+
+        public ICollection<Models.Task> GetTaskInDepartment(int departmentId)
+        {
+            return _db.Tasks.Include(c => c.Department)
+                .Where(c => c.DepartmentId == departmentId).ToList();
+
+        }
+    }
+}
